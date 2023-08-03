f14f2ce6fcfcf1a877f6836746634863c39bd81f
gauridhamal <87119858+gauridhamal@users.noreply.github.com>
2023-08-03 11:15:02 +0530

    Update streamlit_app.py
    try...except clauses added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas 
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('The Streamlit Practice')
 streamlit.header('Breakfast Menu')
@@ -20,14 +21,18 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:  
+    streamlit.error("Please select a food to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  stremlit.error()
+  
 
 streamlit.stop()
 
